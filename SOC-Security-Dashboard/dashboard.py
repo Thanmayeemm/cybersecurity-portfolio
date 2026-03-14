911543aca41cf5c52007b6f98bddaa3ccb8ec55f
Thanmayeemm <manchive@mail.uc.edu>
2026-03-13 23:12:27 -0700

    Update dashboard.py

--- SOC-Security-Dashboard/dashboard.py
+++ SOC-Security-Dashboard/dashboard.py
@@ -1,16 +1,15 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(page_title="SOC Security Monitoring Dashboard", layout="wide")
+
 st.title("SOC Security Monitoring Dashboard")
 
-# Correct path for Streamlit Cloud
-df = pd.read_csv("SOC-Security-Dashboard/security_logs.csv")
+# Correct path
+df = pd.read_csv("security_logs.csv")
 
 st.subheader("Security Event Distribution")
-event_counts = df["event_type"].value_counts()
-st.bar_chart(event_counts)
+st.bar_chart(df["event_type"].value_counts())
 
 st.subheader("Recent Security Events")
 st.dataframe(df.tail(20))
-
-st.info("This dashboard displays simulated SOC security logs.")
